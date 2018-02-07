95ceb53e21f6ffa1ef84fa6118a26fc7c6fc67aa
Felipe <felipepiovezan@wgs.ind.br>
2018-02-07 00:10:14 -0200

    Criado botao para mostrar hora

--- TesteVS2017WinForms/TesteVS2017WinForms/Form1.cs
+++ TesteVS2017WinForms/TesteVS2017WinForms/Form1.cs
@@ -16,5 +16,10 @@ namespace TesteVS2017WinForms
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(DateTime.Now.ToShortTimeString());
+        }
     }
 }
